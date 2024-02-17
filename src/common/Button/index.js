1e5fce6e79a57c79a5ce218f20af736701d5207f
Denis Beresnev <shanthi.bunch@gmail.com>
2024-02-17 18:08:11 +0300

    feat: Buttons and navigation on home page

--- src/common/Button/index.js
+++ src/common/Button/index.js
@@ -2,11 +2,10 @@ import './style.css';
 import { Component } from '@/base';
 
 export default class extends Component {
-  constructor(text, color, onClick) {
+  constructor(text, onClick) {
     super('button');
     this._element.innerText = text;
     this._element.classList.add('button');
-    this._element.style.backgroundColor = color;
     this._element.onclick = onClick;
   }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,19 @@
+import router from '../app/router';
 import { getMenuModule } from '../modules';
+import { Button } from '@/common';
 
 export default () => {
   const page = document.createElement('div');
 
-  page.appendChild(getMenuModule('Menu', []));
+  const connectButton = new Button('p2p', () => {
+    router.navigate('connect');
+  }).element;
+  const offlineButton = new Button('offline').element;
+  offlineButton.disabled = true;
+
+  const menuButtons = [connectButton, offlineButton];
+
+  page.appendChild(getMenuModule('Menu', menuButtons));
 
   return page;
 };
