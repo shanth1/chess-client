a53481eefd48abde1d96b91cb6d1d74eb9a878e2
Denis Beresnev <shanthi.bunch@gmail.com>
2024-02-18 01:20:09 +0300

    feat: Added local chat to game page

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,6 +1,22 @@
+import { Button, Input } from '../common';
+
 export default () => {
   const page = document.createElement('div');
-  page.innerText = 'game';
+  const form = document.createElement('form');
+  const chatContainer = document.createElement('div');
+  const input = new Input().element;
+  const sendButton = new Button('Send', () => {
+    if (input.value) {
+      const message = document.createElement('div');
+      message.innerText = `[${new Date().toUTCString()}]: ${input.value}`;
+      input.value = '';
+      chatContainer.appendChild(message);
+    }
+  }).element;
 
+  form.appendChild(input);
+  form.appendChild(sendButton);
+  page.appendChild(form);
+  page.appendChild(chatContainer);
   return page;
 };
