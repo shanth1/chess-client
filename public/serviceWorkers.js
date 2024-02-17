233501fef834f6d947b9bef9714a1abf856fc486
Denis Beresnev <shanthi.bunch@gmail.com>
2024-02-17 16:33:28 +0300

    feat: Added clearing unnecessary cache

--- public/serviceWorkers.js
+++ public/serviceWorkers.js
@@ -1,16 +1,21 @@
-const cacheKey = 'chess-app-v1';
-const dynamicCacheName = 'dynamic-data';
+const STATIC_CACHE_KEY = 'spa-template-app-v1';
+const DYNAMIC_CACHE_NAME = '';
 
-self.addEventListener('install', async (event) => {
+self.addEventListener('install', async () => {
   try {
-    const cache = await caches.open(cacheKey);
+    const cache = await caches.open(STATIC_CACHE_KEY);
     await cache.addAll(['index.html']);
   } catch (error) {
     console.error('[SW]: INSTALL ERROR', error);
   }
 });
 
-self.addEventListener('activate', () => {});
+self.addEventListener('activate', async () => {
+  const cacheKeys = await caches.keys();
+  (
+    await Promise.all(cacheKeys.filter((name) => name !== STATIC_CACHE_KEY))
+  ).map((name) => caches.delete(name));
+});
 
 self.addEventListener('fetch', async (event) => {
   event.respondWith(networkFirst(event.request));
@@ -19,12 +24,12 @@ self.addEventListener('fetch', async (event) => {
 const networkFirst = async (request) => {
   try {
     const response = await fetch(request);
-    const cached = await caches.open(cacheKey);
+    const cached = await caches.open(STATIC_CACHE_KEY);
     cached.put(request, response.clone());
     return response;
   } catch (error) {
     console.log('network is off, work with cache');
-    const cache = await caches.open(cacheKey);
+    const cache = await caches.open(STATIC_CACHE_KEY);
     const cached = await cache.match(request);
     return cached || Promise.reject('no-cache');
   }
