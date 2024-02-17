caaeb01790c3742fd86504396502a56c36ee220f
Denis Beresnev <shanthi.bunch@gmail.com>
2024-02-17 22:59:38 +0300

    feat: Basic p2p connection with console

--- src/pages/alice.js
+++ src/pages/alice.js
@@ -1,6 +1,37 @@
+import { Button, Input } from '@/common';
+
+const setLocalDescription = async (offer) => {
+  const peerConnection = new RTCPeerConnection();
+  peerConnection.onicecandidate = (e) =>
+    console.log(
+      'icecandidate:',
+      JSON.stringify(peerConnection.localDescription)
+    );
+
+  console.log('before remote:', JSON.parse(offer));
+  peerConnection.setRemoteDescription(JSON.parse(offer));
+
+  let dataChannel;
+  peerConnection.ondatachannel = (event) => {
+    console.log('ON DATA CHANNEL');
+    dataChannel = event.channel;
+    dataChannel.onopen = () => console.log('Канал получен и открыт!');
+    dataChannel.onmessage = (e) => console.log('message:', e.data);
+  };
+
+  const answer = await peerConnection.createAnswer();
+  peerConnection.setLocalDescription(answer);
+};
+
 export default () => {
   const page = document.createElement('div');
-  page.innerText = 'Alice';
+  const input = new Input().element;
+  const button = new Button('Ok', () => {
+    setLocalDescription(input.value);
+  }).element;
+
+  page.appendChild(input);
+  page.appendChild(button);
 
   return page;
 };
--- src/pages/bob.js
+++ src/pages/bob.js
@@ -1,7 +1,40 @@
+import { Button, Input } from '@/common';
+
+const setLocalDescription = async (peerConnection) => {
+  const dataChannel = peerConnection.createDataChannel('channel-name');
+
+  dataChannel.onopen = () => console.log('Канал открыт');
+  dataChannel.onmessage = (e) => console.log('message:', e.data);
+
+  const offer = await peerConnection.createOffer();
+
+  peerConnection.setLocalDescription(offer);
+
+  peerConnection.onicecandidate = (e) => {
+    console.log(
+      'icecandidate:',
+      JSON.stringify(peerConnection.localDescription)
+    );
+  };
+};
+
+const setRemoteDescription = (peerConnection, answer) => {
+  peerConnection.setRemoteDescription(JSON.parse(answer));
+};
+
 export default () => {
   const page = document.createElement('div');
-  console.log(1);
-  page.innerText = 'bob';
+  const peerConnection = new RTCPeerConnection();
+
+  setLocalDescription(peerConnection);
+
+  const input = new Input().element;
+  const button = new Button('Ok', () => {
+    setRemoteDescription(peerConnection, input.value);
+  }).element;
+
+  page.appendChild(input);
+  page.appendChild(button);
 
   return page;
 };
