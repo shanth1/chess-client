0c414d4afd6845dca55dfed5c8a7f7169be4e263
Denis Beresnev <shanthi.bunch@gmail.com>
2024-02-24 13:17:03 +0300

    feat: Chess connection by RTC and legal move control

--- src/modules/Chess/index.js
+++ src/modules/Chess/index.js
@@ -1,14 +1,60 @@
+import { Chess } from 'chess.js';
+import { connection } from '../../app/connection';
 import styles from './style.module.css';
 
 const initBoard = () => {
   setTimeout(() => {
-    Chessboard('board', {
+    function onDragStart(_, piece) {
+      if (game.isGameOver()) return false;
+
+      if (
+        (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)
+      ) {
+        return false;
+      }
+    }
+
+    function onDrop(source, target) {
+      try {
+        var move = game.move({
+          from: source,
+          to: target,
+          promotion: 'q',
+        });
+        if (connection.dataChannel) {
+          const movePresentation = JSON.stringify(move);
+          connection.dataChannel.send(movePresentation);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+
+    function onSnapEnd() {
+      board.position(game.fen());
+    }
+
+    const game = new Chess();
+    const board = Chessboard('board', {
       pieceTheme: 'assets/{piece}.png',
       position: 'start',
       draggable: true,
       dropOffBoard: 'trash',
       sparePieces: false,
+      onDragStart,
+      onDrop,
+      onSnapEnd,
     });
+
+    if (connection.dataChannel) {
+      connection.dataChannel.onmessage = (event) => {
+        const move = JSON.parse(event.data);
+        console.log(move);
+        game.move(move);
+        board.position(game.fen());
+      };
+    }
   });
 };
 
